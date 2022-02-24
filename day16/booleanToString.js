d8da86b4d3556b1fdcaa89069fb7b9b8f71eb0b4
ruro122020 <ruro122020@gmail.com>
2022-02-24 16:52:57 -0700

    day16

---
+++ day16/booleanToString.js
@@ -0,0 +1,10 @@
+/*
+Implement a function which convert the given boolean value into its string representation.
+*/
+
+function booleanToString(b) {
+  return b.toString();
+}
+
+booleanToString("true");
+booleanToString("false");
